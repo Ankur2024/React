7d2058edde3c19b1ece50261f479100784acacc3
Ankur2024 <ag3942280@gmail.com>
2024-07-21 16:42:19 +0530

    Context Api Done

--- Theme-switcher/context/Theme.js
+++ Theme-switcher/context/Theme.js
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-
+   
 
 export const ThemeContext = createContext({
     themeMode: "Light",
@@ -7,8 +7,16 @@ export const ThemeContext = createContext({
     lightTheme: () => {},
 })
 
+/**
+ * Provides the ThemeContext.Provider component, which can be used to wrap components
+ * that need access to the theme context.
+ */
 export const ThemeProvider = ThemeContext.Provider
 
+/**
+ * A custom React hook that provides access to the theme context.
+ * @returns {Object} An object containing the current theme mode and functions to switch between light and dark themes.
+ */
 export default function useTheme(){
     return useContext(ThemeContext);
 }
