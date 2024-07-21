120e7d588beffb934615c0e2b97103527cfde868
Ankur2024 <ag3942280@gmail.com>
2024-07-21 12:19:08 +0530

    React router dom Complete

--- react-router/src/components/UserDetails.js
+++ react-router/src/components/UserDetails.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
+
 
 export default function UserDetails() {
+  const params = useParams()
+  const userId  = params.userId;
   return (
-    <div>UserDetails</div>
+    <div>UserDetails {userId}</div>
   )
 }
